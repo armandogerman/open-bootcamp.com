2f32a95ccf5507da6c66f8afce4304777337f5de
Armando Fava <armandogerman@gmail.com>
2022-08-12 11:19:07 -0300

    Ejercicio 5

---
+++ Ejercicio_5.py
@@ -0,0 +1,11 @@
+Nombre="Juan"
+Apellidos="Perez"
+Edad=34
+Email="jp@mail.com"
+Telefono="23423424"
+Casado=False
+Hijos=False
+amigos=["Pedro","Pablo"]
+Peliculas={1:'Magadascar', 2:'Shrek'}
+print(Nombre,Apellidos,Edad,Email,Telefono,Casado,Hijos,amigos,Peliculas)
+print(type(Nombre),type(Apellidos),type(Edad),type(Email),type(Telefono),type(Casado),type(Hijos),type(amigos),type(Peliculas))
