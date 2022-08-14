6151de2a97d6f42f1358eb79cc7ba55b9fbeea2b
Armando Fava <armandogerman@gmail.com>
2022-08-14 19:03:22 -0300

    Ejercicio 15

---
+++ Ejercicio_15.py
@@ -0,0 +1,11 @@
+# En este ejercicio tendréis que crear un módulo que contenga las operaciones básicas 
+# de una calculadora: sumar, restar, multiplicar y dividir.
+# Este módulo lo importaréis a un archivo python y llamaréis a las funciones creadas. 
+# Los resultados tenéis que mostrarlos por consola.
+
+import operaciones as o
+
+print(o.suma(2,5))
+print(o.resta(2,5))
+print(o.multiplicar(2,5))
+print(o.dividir(2,5))
